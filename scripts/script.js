4066e2171f3b87579c937d518f4dd1f6039e2517
Lucas Hideki Minamihara dos Santos <lucas@Jobcenter.net>
2022-10-24 10:36:28 -0300

    Finished Game

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
+  const minimumMarginDeath = 694;
+  const maximumMarginDeath = 850;
+  const minimumJump = 60;
+  let actualPersonPosition;
+  let actualPersonPositionJump;
   let keyBoardKey = "";
   const person = window.document.querySelector(".person");
 
   // minumum value margin 680px
   // maximum margin 900px
-  setInterval(() => {
-    console.log(getComputedStyle(person).margin);
-  }, 200);
+
+  const runDetector = setInterval(() => {
+    actualPersonPosition = parseInt(window.getComputedStyle(person).marginLeft);
+    actualPersonPositionJump = parseInt(
+      window.getComputedStyle(person).marginBottom
+    );
+    console.log(actualPersonPositionJump);
+    if (
+      actualPersonPosition >= minimumMarginDeath &&
+      actualPersonPosition <= maximumMarginDeath &&
+      actualPersonPositionJump <= minimumJump
+    ) {
+      person.style.animationPlayState = "paused";
+      person.style.backgroundColor = "blue";
+      clearInterval(runDetector);
+    }
+  }, 100);
 
   window.addEventListener("keydown", (event) => {
     keyBoardKey = event.key;
